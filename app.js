5ec5793aef7293eba83725772854b35bcc812268
CodingWithConker <kenneth.faulkner28@yahoo.com>
2022-03-18 10:48:49 -0700

    Added constructors and an event listener

---
+++ app.js
@@ -0,0 +1,17 @@
+// ES5 Version
+
+// Book Constructor
+function Book(title, author, isbn){
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+// UI Constructor
+function UI(){}
+
+// Event Listeners
+document.getElementById('book-form').addEventListener('submit', function(e){
+    console.log('test');
+    e.preventDefault();
+});
